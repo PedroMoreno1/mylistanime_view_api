3ff88a1c6c32e6fccb06837bc5282e38eab9cd26
PedroMoreno1 <pedromoreno.pz1@gmail.com>
2025-07-16 16:07:48 -0500

    Primer commit - Un nuevo inicio

---
+++ src/main/java/com/mylistanime/MylistanimeViewApiApplication.java
@@ -0,0 +1,13 @@
+package com.mylistanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylistanimeViewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylistanimeViewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mylistanime/MylistanimeViewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylistanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylistanimeViewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
